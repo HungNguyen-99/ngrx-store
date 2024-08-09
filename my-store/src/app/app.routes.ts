4867d5c92c89ccf8d31c1aaf4d38c839fa9eab25
DESKTOP-GUCA87P\admin <hungnvhe130726@gmail.com>
2024-08-09 07:30:57 +0700

    yes

--- my-store/src/app/app.routes.ts
+++ my-store/src/app/app.routes.ts
@@ -2,6 +2,11 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
+    path: '',
+    redirectTo: 'app-walkthrough',
+    pathMatch: 'full',
+  },
+  {
     path: 'app-walkthrough',
     loadComponent: () =>
       import('./components/walkthrough/walkthrough.component').then(
@@ -13,6 +18,13 @@ export const routes: Routes = [
     loadComponent: () =>
       import('./components/overview-effect/overview-effect.component').then(
         (m) => m.OverviewEffectComponent
+      ),
+  },
+  {
+    path: 'my-component-store',
+    loadComponent: () =>
+      import('./components/component-store/my-component-store.component').then(
+        (m) => m.MyComponentStoreComponent
       ),
   },
 ];
---
+++ my-store/src/app/components/component-store/my-component-store.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-component-store',
+  standalone: true,
+  template: `<h1>My Component Store</h1>`,
+  imports: [],
+})
+export class MyComponentStoreComponent {}
